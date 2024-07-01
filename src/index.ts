ba6b8cc0bee5e79dbdc8efe8ece5d93534835bdd
vedant <22030070@ycce.in>
2024-07-02 00:53:11 +0530

    prisma setupdone

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { query }  from "express";
 
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from "./lib/db";
 
 async function  init() {
   const app= express();
@@ -10,10 +11,16 @@ const PORT= Number(process.env.PORT) || 3000;
 
 const gqlServer = new ApolloServer({
   typeDefs : `
+
   type Query {
     hello: String,
     say(say:String): String
   }
+
+  type Mutation{
+  createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
+  }
+
 `,
 
 // Define your resolvers
@@ -21,8 +28,24 @@ resolvers : {
   Query: {
     hello: () => 'hi buddy',
     say: (_ , {say}:{say:String}) => `${say}`
-  }
-}
+  },
+   Mutation: {
+    createUser:async (_,{firstName,lastName,password,email}:{firstName:string,lastName:string,email:string,password:string})=>{
+      await prismaClient.user.create({
+        data : {
+          email,
+          firstName,
+          lastName,
+          password,
+          salt :"random Salt",
+          ProfileImageURL : 'http://example.com/profile.jpg',
+        },
+       
+      });
+      return true;
+    },
+   },
+},
 });
 
 await gqlServer.start();
